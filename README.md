1cac1623c5f92335ad342f7d51fe2e8e99583df0
John Lockwood <elitepropertiesbroker@gmail.com>
2022-06-27 07:57:13 -0400

    Recent changes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Managing WordPress Content With Python
 
+Todo:
+
+* Figure out sort order; make async or multi-threaded. Currently sorted by last modified descending. That's probably OK, get top 20 or so?
+* W
---
+++ test.py
@@ -0,0 +1,47 @@
+import configparser
+import requests
+import httpx
+from json import dump
+from pprint import pp
+
+def get_config():
+    config = configparser.ConfigParser()
+    config.read('blog.ini')
+    return config["main"]
+
+def wp_url(url, page):
+    """Returns the URL and params for a single page of posts"""
+    url = f"{url}/wp-json/wp/v2/posts?orderby=modified&order=desc"
+    params = {"page": page}
+    return url, params
+
+def get_posts(url):
+    all_posts = []
+    page = 1
+    while True:    
+        url, params = wp_url(base_url, page)
+        page = page + 1
+        r = httpx.get(url, params=params)
+        if r.status_code != 200:
+            break        
+        json = r.json()
+        all_posts = all_posts + json
+    return all_posts
+
+config = get_config()
+base_url = config.get("blog_url")
+
+posts = get_posts(base_url)
+with open('all_posts_ordered.json', mode='wt') as f:
+    dump(posts, f)
+# print("Getting posts for the URL defined in blog.ini")
+# print("This may take some time.  Please wait...")
+# for post in get_posts(base_url):
+#     # print(post["link"])
+#     pp(post)
+
+# print(f"Posts retrieved: {len(all_posts)}")
+# d = all_posts[0].copy()
+# del(d["content"])
+#pp(d)
+
