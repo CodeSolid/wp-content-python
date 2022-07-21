c442d79d2f82ee61c4bc427e4f1c7b451338e111
John Lockwood <elitepropertiesbroker@gmail.com>
2022-07-21 08:48:21 -0400

    IN progress -- stopping point for debugging demo?

--- get_posts.py
+++ get_posts.py
@@ -1,24 +1,29 @@
-import configparser
 import requests
 from json import dump
 from pprint import pp
 
-def get_config():
-    config = configparser.ConfigParser()
-    config.read('blog.ini')
-    return config["main"]
+BLOG_URL = "https://codesolid.com"
 
-def wp_url(url, page):
+
+def posts_url(url, page):
     """Returns the URL and params for a single page of posts"""
     url = f"{url}/wp-json/wp/v2/posts?orderby=date&order=desc"
     params = {"page": page}
     return url, params
 
+def get_categories(url):
+    url = f"{url}/wp-json/wp/v2/categories"
+    r = requests.get(url)
+    if r.status_code != 200:
+        raise Exception("Unable to get post categories")
+    json = r.json()
+    print(json)
+
 def get_posts(url):
     all_posts = []
     page = 1
     while True:    
-        url, params = wp_url(base_url, page)
+        url, params = posts_url(url, page)
         page = page + 1
         r  = requests.get(url, params=params)
         if r.status_code != 200:
@@ -27,23 +32,17 @@ def get_posts(url):
         all_posts = all_posts + json
     return all_posts
 
-config = get_config()
-base_url = config.get("blog_url")
 
-posts = get_posts(base_url)
-with open('all_posts_ordered.json', mode='wt') as f:
-    dump(posts, f)
-for post in posts:
-   print(f'{post["date"][0:10]},{post["link"]}')
 
-# print("Getting posts for the URL defined in blog.ini")
-# print("This may take some time.  Please wait...")
-# for post in get_posts(base_url):
-#     # print(post["link"])
-#     pp(post)
+def format_list(items):
+    return ";".join(items)
 
-# print(f"Posts retrieved: {len(all_posts)}")
-# d = all_posts[0].copy()
-# del(d["content"])
-#pp(d)
+# posts = get_posts(BLOG_URL)
+# with open('all_posts_ordered.json', mode='wt') as f:
+#     dump(posts, f)
 
+
+# for post in posts:
+#    print(f'{post["date"][0:10]},{post["link"]}')
+
+get_categories(BLOG_URL)
