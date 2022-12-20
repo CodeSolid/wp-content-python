aef9262c94552825177c0ba4234259c63d986d0d
John Lockwood <elitepropertiesbroker@gmail.com>
2022-12-20 15:47:34 -0500

    Latest changes before making async

--- update.sh
+++ update.sh
@@ -1,3 +1,6 @@
 
 python download.py && \
 python process_downloaded.py
+cd reports
+jupyter nbconvert --to notebook --execute PostStatistics.ipynb
+mv PostStatistics.nbconvert.ipynb PostStatistics.ipynb
